862d8998f317d7282bfc60696eaf80e87b79c1ac
Michal Hoftich <michal.h21@gmail.com>
2025-04-10 22:23:49 +0200

    fixní menu, zavírej menu

--- tex/js/toc.js
+++ tex/js/toc.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
       event.preventDefault(); // zabránit otevření stránky
       const chapterTocs = chapterToc.parentNode.querySelectorAll('span.chapterToc');
       chapterTocs.forEach(function(otherChapterToc) {
-        otherChapterToc.classList.remove('clicked');
+        if (otherChapterToc !== chapterToc) {
+          otherChapterToc.classList.remove('clicked');
+        }
       });
       const sectionTocs = chapterToc.parentNode.querySelectorAll('span.sectionToc');
       sectionTocs.forEach(function(sectionToc) {
