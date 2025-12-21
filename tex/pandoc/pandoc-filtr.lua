a8ac75c91be1cf781a6e50214a282c60855ab977
Michal Hoftich <michal.h21@gmail.com>
2025-12-21 21:40:05 +0100

    new vimwiki tagging support

--- tex/pandoc/pandoc-filtr.lua
+++ tex/pandoc/pandoc-filtr.lua
@@ -1,3 +1,30 @@
+-- mapování speciálních znaků použitých v tagech ve vimwiki na příkazy LaTeXu
+local command_map = {
+  ["@"] = "place", -- pro města, regiony a podobně
+  [">"] = "author", -- 
+}
+local function get_tag(text)
+  -- vytvořil jsem si různý kategorie tagů pro vimwiki, ty se musej mapovat na různý příkazy v LaTeXu
+  local invisible = ""
+  -- pokud tag začíní znakem !, je to neviditelný tag, takže by se neměl vypsat ve výsledným dokumentu, je určenej pro interní použití
+  if text:match("^%!") then
+    -- neviditelný příkazy mají prefix invisible
+    invisible = "invisible"
+    text = text:sub(2)
+  end
+  local first_char = text:sub(1,1)
+  if command_map[first_char] then
+    local command = command_map[first_char]
+    text = text:sub(2)
+    return "\\tag" .. invisible .. command .. "{" .. text .. "}"
+  else
+    return "\\tag" .. invisible .. "basic" .. "{" .. text .. "}"
+  end
+
+
+
+end
+
 function Span(el)
   -- zahodit interní negativní vimwiki tag
   if el.identifier
@@ -11,6 +38,6 @@ function Span(el)
   -- normální vimwiki tag
   if el.classes:includes("tag") then
     local text = pandoc.utils.stringify(el.content)
-    return pandoc.RawInline("latex", "\\vwtag{" .. text .. "}")
+    return pandoc.RawInline("latex", get_tag(text))
   end
 end
